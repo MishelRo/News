947898a4dc48dec028cfeaa95c0c5d51d8983b26
Mishel <mir89@mail.ru>
2020-12-08 22:50:12 +0400

    Initial Commit

---
+++ FreshNews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FreshNews
+//
+//  Created by User on 08.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
